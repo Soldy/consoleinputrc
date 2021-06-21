4f835c44ffe35cb371b2681c30b0e189bfcbf886
Soldy <4786022+Soldy@users.noreply.github.com>
2021-06-21 23:58:23 +0100

    set move
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,18 +2,29 @@ require('theuniverse');
 const $universe = global.theUn1v3rse.controls.interface();
 const $stdio = $universe.req('sdio','consolestdiorc', 'base');
 const $watch = $universe.req('watch', 'consolewatchrc', 'base');
-const $box = $universe.req('watch', 'consoleboxrc', 'base');
+const $box = new (require('consoleboxrc')).base();
 
 
 const inputBase = function(){
-
+    this.setup = function(){
+        return $box.setup();
+    }
+    this.set = function(name, value){
+        $box.set(set,value);
+    }
+    this.move = function(){
+        $box.clean();
+        $box.move();
+        $box.print();
+    }
     let _x = 0;
     let _y = 0;
     let _insert = false;
     let _buffer = '';
     let _position = 0;
     const _render = function(){
-
+         $box.clear();
+         $box.add(_buffer);
     }
     const _event = function(character){
         if(_position > _buffer.length)
@@ -32,6 +43,7 @@ const inputBase = function(){
         _buffer = arr.join('');
         return render();
     }
+    $box.rc.set()
 }
 
 
